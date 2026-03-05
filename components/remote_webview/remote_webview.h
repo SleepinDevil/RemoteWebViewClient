f4613a982aab39a4cfd68c42bd54820523092d46
SleepinDevil <sleepindevil@hotmail.com>
2026-03-05 17:43:16 +1100

    Update remote_webview.h

--- components/remote_webview/remote_webview.h
+++ components/remote_webview/remote_webview.h
@@ -56,7 +56,7 @@ class RemoteWebView : public Component {
 
   // Automation on_update_frame things
   void add_on_state_callback(std::function<void(bool)> &&callback);
-  void trigger_on_frame_update(bool state);
+  void frame_update_trigger(bool state);
   bool state{false};
  private:
   struct WsMsg {
