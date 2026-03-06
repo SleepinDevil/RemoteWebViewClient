f7a234f572a19946aebea3d466e6f4045474e712
SleepinDevil <sleepindevil@hotmail.com>
2026-03-06 13:34:33 +1100

    Update automation.h
    Updated the on_frame_update trigger to be state-less since there was no need to do it using the example template approach. This works much nicer

--- components/remote_webview/automation.h
+++ components/remote_webview/automation.h
@@ -7,7 +7,22 @@
 namespace esphome {
 namespace remote_webview {
 
-template<typename... Ts> class OnFrameUpdateSetStateAction : public Action<Ts...> {
+// Automation things below
+// MODIFIED: Renamed to TriggerOnFrameUpdateAction to match what it does. 
+// Removed TEMPLATABLE_VALUE(bool, state) as it requires no arguments now.
+template<typename... Ts> class TriggerOnFrameUpdateAction : public Action<Ts...> {
+ public:
+  explicit TriggerOnFrameUpdateAction(RemoteWebView *ea) : ea_(ea) {}
+
+  void play(Ts... x) override {
+    // MODIFIED: We just call our component's trigger method directly.
+    this->ea_->trigger_on_frame_update();
+  }
+
+ protected:
+  RemoteWebView *ea_;
+};
+/*template<typename... Ts> class OnFrameUpdateSetStateAction : public Action<Ts...> {
  public:
   explicit OnFrameUpdateSetStateAction(RemoteWebView *ea) : ea_(ea) {}
   TEMPLATABLE_VALUE(bool, state)
@@ -19,7 +34,7 @@ template<typename... Ts> class OnFrameUpdateSetStateAction : public Action<Ts...
 
  protected:
   RemoteWebView *ea_;
-};
+};*/
 
 // Trying to cull the condition part of the automation since we don't need that
 /*
@@ -33,12 +48,21 @@ template<typename... Ts> class OnFrameUpdateCondition : public Condition<Ts...> 
   bool state_;
 }; */
 
-class OnFrameUpdateStateTrigger : public Trigger<bool> {
+// MODIFIED: Renamed to OnFrameUpdateTrigger. Changed 'Trigger<bool>' to 'Trigger<>' 
+// because we don't need to pass a boolean along with the trigger anymore.
+class OnFrameUpdateTrigger : public Trigger<> {
+ public:
+  explicit OnFrameUpdateTrigger(RemoteWebView *parent) {
+    // MODIFIED: Use our new parameterless callback manager.
+    parent->add_on_frame_update_callback([this]() { this->trigger(); });
+  }
+};
+/*class OnFrameUpdateStateTrigger : public Trigger<bool> {
  public:
   explicit OnFrameUpdateStateTrigger(RemoteWebView *parent) {
     parent->add_on_state_callback([this](bool state) { this->trigger(state); });
   }
-};
+};*/
 
 }  // namespace remote_webview
 }  // namespace esphome
